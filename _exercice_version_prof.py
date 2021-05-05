63d1e738ab7e5d00e44b90c3ca078c08b9ad6fee
kegaub <kegaub@users.noreply.github.com>
2021-05-05 08:11:16 -0400

    Update _exercice_version_prof.py

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    nouveau_mot = ""
-    for lettre in mot:
-        if 97 <= ord(lettre) <= 122 or 224 <= ord(lettre) <= 246:
-            nouveau_mot += chr(ord(lettre) - 32)
-        else:
-            nouveau_mot += lettre
-
-    return nouveau_mot
+    pass
 
 
 if __name__ == '__main__':
