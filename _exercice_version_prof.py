e8227cd724d357f771e0539137091c9d40ab79ac
kegaub <kevin.gauthier.22.06@gmail.com>
2021-05-11 16:58:57 -0400

    Update _exercice_version_prof.py

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    pass
+    nouveau_mot = ""
+    for lettre in mot:
+        if 97 <= ord(lettre) <= 122 or 224 <= ord(lettre) <= 246:
+            nouveau_mot += chr(ord(lettre) - 32)
+        else:
+            nouveau_mot += lettre
+
+    return nouveau_mot
 
 
 
